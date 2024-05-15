6f144d7e5a01fd7973f440008cf013a46af2ef26
rungulus <14097023+rungulus@users.noreply.github.com>
2024-05-14 22:24:31 -0600

    count up

--- src/components/Countdown.svelte
+++ src/components/Countdown.svelte
@@ -1,14 +1,13 @@
 <script>
-  const countDownDate = new Date("May 15, 2024 00:00:00").getTime();
-  //May 15th according to RT Email
+  const startDate = new Date("May 15, 2024 00:00:00").getTime();
 
   let countdownTimer;
-  let timeLeft = "Loading...";
+  let timeElapsed = "Loading...";
   let increment = "";
 
   function countdownClock() {
     const now = new Date().getTime();
-    const distance = countDownDate - now;
+    const distance = now - startDate;
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -16,32 +15,22 @@
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    if (days > 0) {
-      clearInterval(countdownTimer); //we don't need to run this each second if there are days left
-      timeLeft = days;
-      increment = "days left of Rooster Teeth";
-    } else if (hours > 0) {
-      timeLeft = hours;
-      increment = "hours left of Rooster Teeth";
-    } else if (minutes > 0) {
-      timeLeft = minutes;
-      increment = "minutes left of Rooster Teeth";
-    } else if (seconds > 0) {
-      timeLeft = seconds;
-      increment = "seconds left of Rooster Teeth :(";
-    }
-    if (distance < 0) {
+    if (days >= 0) {
+      clearInterval(countdownTimer); //we don't need to run this each second if there are days elapsed
+      timeElapsed = days;
+      increment = "days since we were here";
+    } else {
       clearInterval(countdownTimer);
-      timeLeft = "0 days left :(<br>RIP Rooster Teeth";
+      timeElapsed = "0 days since we were here";
       increment = "";
     }
   }
   countdownTimer = setInterval(countdownClock(), 1000);
 </script>
 
-<p class="text-center fs-1 runguszone">
-  <span class="badge text-bg-danger"
-    ><span class="font-weight-bold" id="time">{timeLeft}</span>
+<p class="text-center fs-5 runguszone">
+  <span class="badge text-bg-info"
+    ><span class="font-weight-bold" id="time">{timeElapsed}</span>
     <span id="increment">{increment}</span></span
   >
 </p>
